46f28ce82033e3004c536c90c69362c6417258f4
Danila <asd@mail.ru>
2023-04-16 14:39:09 +0300

    Добавьте файлы проекта.

---
+++ junior17/Program.cs
@@ -0,0 +1,16 @@
+﻿Random rand = new Random();
+int randomNumber = rand.Next(100);
+
+int degree = 0;
+int totalCount = 1;
+int multiplicationNumber = 2;
+
+Console.WriteLine("Заданное число: " + randomNumber);
+
+while (totalCount <= randomNumber)
+{
+    degree++;
+    totalCount *= multiplicationNumber;
+}
+
+Console.WriteLine($"{multiplicationNumber} в степени {degree} = {totalCount}");
